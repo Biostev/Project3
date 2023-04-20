993734976d9dc412c8e7b7e149b56fa3b1841236
Biostev <KONAKOV06@mail.ru>
2023-04-21 01:27:45 +0300

    1. Profile page added 2. Edit mode for profile page added 3. Users' avatars added

--- data/users.py
+++ data/users.py
@@ -2,7 +2,8 @@ import datetime
 
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from sqlalchemy import Column, Integer, String, DateTime
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from sqlalchemy import Column, Integer, String, DateTime, BLOB
 from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import PasswordField, StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
@@ -18,6 +19,9 @@ class User(SqlAlchemyBase, UserMixin):
     email = Column(String, index=True, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_date = Column(DateTime, default=datetime.datetime.now)
+    with open('static/img/default_avatar.png', 'rb') as file:
+        default_avatar = file.read()
+    avatar = Column(BLOB, default=default_avatar)
 
     def __init__(self, name: str, email: str, password: str):
         self.name = name
@@ -30,13 +34,30 @@ class User(SqlAlchemyBase, UserMixin):
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
+    def set_avatar(self, new_avatar):
+        with open(new_avatar, 'rb') as avatar:
+            self.avatar = avatar.read()
+
+    def change_name(self, new_name):
+        self.name = new_name
+
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('Никнейм', validators=[DataRequired()])
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    submit = SubmitField('Зарегистрироваться')
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    avatar = FileField(
+        'Avatar',
+        validators=[FileAllowed(['jpg', 'png'], 'Images only!')],
+    )
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import datetime
 import os
+import base64
+from PIL import Image
+import io
 
-from flask import (Flask, render_template, redirect)
+from flask import (Flask, render_template, redirect, request)
 from flask_login import (LoginManager, login_user, login_required,
                          logout_user, current_user)
 from flask_wtf import FlaskForm
@@ -8,9 +12,11 @@ from wtforms import EmailField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 from data import db_session
-from data.users import User, RegisterForm
+from data.users import User, RegisterForm, EditProfileForm
 
-from DBCreator import init_db
+from werkzeug.utils import secure_filename
+
+# from DBCreator import init_db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -28,13 +34,69 @@ class LoginForm(FlaskForm):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
+
+
+@app.route('/profile')
+def profile():
+    form = EditProfileForm()
+    date = calculate_date()
+    avatar = make_avatar(current_user)
+    return render_template(
+        'profile.html',
+        avatar=avatar, date=date[0], days=date[1],
+        edit_mode=False,
+        form=form
+    )
+
+
+@app.route('/profile/edit', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm(
+        name=current_user.name,
+        email=current_user.email
+    )
+
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+
+        same_name = db_sess.query(User).filter(User.email == form.email.data).first()
+        same_email = db_sess.query(User).filter(User.name == form.name.data).first()
+        if same_name and same_name != current_user:
+            return render_template('profile.html', title='Регистрация',
+                                   form=form,
+                                   edit_mode=True,
+                                   message='This name already exists')
+        if same_email and same_email != current_user:
+            return render_template('profile.html', title='Регистрация',
+                                   form=form,
+                                   edit_mode=True,
+                                   message='This email already exists')
+
+        user = db_sess.query(User).filter(User.email == current_user.email).first()
+        user.name = form.name.data
+        user.email = form.email.data
+        loaded_avatar = request.files['avatar']
+        if loaded_avatar.filename:
+            avatar_name = secure_filename(loaded_avatar.filename)
+            loaded_avatar.save(f'uploaded/{avatar_name}')
+            with open(f'uploaded/{avatar_name}', 'rb') as file:
+                avatar = file.read()
+            user.avatar = avatar
+        db_sess.commit()
+
+        return redirect('/profile')
+    return render_template(
+        'profile.html', title='Profile edit',
+        edit_mode=True,
+        form=form
+    )
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -61,7 +123,7 @@ def register():
         db_sess.commit()
 
         return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+    return render_template('register.html', title='Registry', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -74,7 +136,7 @@ def login():
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
-                               message="Неправильный логин или пароль",
+                               message="Invalid login or password",
                                form=form)
     return render_template('login.html', title='Authorization', form=form)
 
@@ -86,10 +148,28 @@ def logout():
     return redirect("/")
 
 
+def calculate_date():
+    date = current_user.created_date.date()
+
+    end = datetime.datetime.now().date()
+
+    diff = end - date
+
+    days = int(diff.total_seconds() // 86400)
+    return date, days
+
+
+def make_avatar(user):
+    avatar = user.avatar
+    avatar = base64.b64encode(avatar)
+    avatar = avatar.decode("UTF-8")
+    return avatar
+
+
 def main():
     db_session.global_init("db/GameManager.db")
-    init_db()  # this is used to create db
-    # app.run()
+    # init_db()  # this is used to create db
+    app.run()
 
 
 if __name__ == '__main__':
